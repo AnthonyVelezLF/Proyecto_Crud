ccd5b176fd402d52fcb025d5d50d36360221b3c1
Anth8ny LF <86171664+AnthonyVelezLF@users.noreply.github.com>
2022-01-25 06:55:34 -0500

    Update cargo.py

--- cargo.py
+++ cargo.py
@@ -1,31 +1,11 @@
-from categorias import Categoria
-
-
-class Articulo:
+class Cargo:
   secuencia=2             
-  articulos = [ {"codigo":1,"descripcion":"Coca Cola","categoria":2,"precio":1.5,"stock":100},
-              {"codigo":2,"descripcion":"Vita Leche","categoria":1,"precio":1,"stock":50},
-             ]
+  cargos = [ {"codigo":1,"cargo":"Analista"},{"codigo":2,"cargo":"Jefe"}]
 
-  def __init__(self,descrip,codCategoria,precio,stock):
-    Articulo.secuencia +=1
-    self.codigo=Articulo.secuencia
+  def __init__(self,descrip):
+    Cargo.secuencia +=1
+    self.codigo=Cargo.secuencia
     self.descripcion=descrip
-    self.categoria=codCategoria
-    self.precio=precio
-    self.stock=stock
-  
-    
-  def mostrar(self):
-    print("{} - {} - {} - {} - {} ".format(self.codigo,self.descripcion,self.categoria,self.precio,self.stock))
 
   def registro(self):
-    return {"codigo":self.codigo,"descripcion":self.descripcion,"categoria":self.categoria,"precio":self.precio,"stock":self.stock}
-#print(Articulo.articulos)
-
-# art = Articulo("Pepsi",2,1.5,100)
-# #art.mostrar()
-# articulo = art.registro()
-# #print(articulo)
-# Articulo.articulos.append(articulo)
-# print(Articulo.articulos)
+    return {"codigo":self.codigo,"cargo":self.descripcion}
